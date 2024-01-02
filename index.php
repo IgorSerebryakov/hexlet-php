21eaf9877af3d2dc5676da5789c0f6a202897427
IgorSerebryakov <serebryakov09@mail.ru>
2024-01-03 00:16:44 +0300

    example for tree

--- index.php
+++ index.php
@@ -1,8 +1,55 @@
 <?php
 
-
 require_once __DIR__ . '/vendor/autoload.php';
 
-use Hexlet\Php\Runner;
+use function Php\Immutable\Fs\Trees\trees\mkfile;
+use function Php\Immutable\Fs\Trees\trees\mkdir;
+use function Php\Immutable\Fs\Trees\trees\isFile;
+use function Php\Immutable\Fs\Trees\trees\isDirectory;
+use function Php\Immutable\Fs\Trees\trees\getName;
+use function Php\Immutable\Fs\Trees\trees\getChildren;
+use function Php\Immutable\Fs\Trees\trees\getMeta;
+use function Php\Immutable\Fs\Trees\trees\array_flatten;
 
-print_r(Runner\run());
+$tree = mkdir('/', [
+    mkdir('etc', [
+        mkdir('apache'),
+        mkdir('nginx', [
+            mkfile('nginx.conf'),
+        ]),
+        mkdir('consul', [
+            mkfile('config.json'),
+            mkdir('data'),
+        ]),
+    ]),
+    mkdir('logs'),
+    mkfile('hosts'),
+]);
+
+function findEmptyDirPaths($tree, $ancestry, $acc)
+{
+    $name = getName($tree);
+    $newAncestry = ($name === '/') ? '' : "$ancestry/$name";
+    $children = getChildren($tree);
+
+
+    if (count($children) === 0) {
+        $acc[] = $newAncestry;
+        return $acc;
+    }
+
+    $dirNames = array_filter($children, fn($child) => !isFile($child));
+
+    $emptyDirNames = array_reduce(
+        $dirNames,
+        function ($newAcc, $child) use ($newAncestry) {
+            return findEmptyDirPaths($child, $newAncestry, $newAcc);
+        },
+        $acc
+    );
+
+    $result = array_flatten($emptyDirNames);
+    return $result;
+}
+
+print_r(findEmptyDirPaths($tree, '', []));
